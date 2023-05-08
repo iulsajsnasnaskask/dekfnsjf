41d29a3c74904cb753a15f92542fbffc4a0a98ab
iulsajsnasnaskask <116762658+iulsajsnasnaskask@users.noreply.github.com>
2023-05-09 00:41:26 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+      setTimeout(() => {
+            let container = document.querySelector('.container')
+            let body = document.querySelector('body')
+            let loader = document.querySelector('.loaderev')
+            loader.style.display = 'none'
+            container.style.display = "flex";
+            body.setAttribute('class', 'afterload')
+      }, 500)
