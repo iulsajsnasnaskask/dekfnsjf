0766fd7835ed31f2cfdfbab372bd14b2876fb600
iulsajsnasnaskask <116762658+iulsajsnasnaskask@users.noreply.github.com>
2023-06-29 15:42:17 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-      setTimeout(() => {
-            let container = document.querySelector('.container')
-            let body = document.querySelector('body')
-            let loader = document.querySelector('.loaderev')
-            loader.style.display = 'none'
-            container.style.display = "flex";
-            body.setAttribute('class', 'afterload')
-      }, 1000)
